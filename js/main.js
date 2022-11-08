a57395cb94754a69065ab9ff2d1caefba0d52883
TUNG N KIM <Tungkims@gmail.com>
2022-11-08 11:53:36 -0800

    Init commit

---
+++ js/main.js
@@ -0,0 +1,184 @@
+var picker = datepicker("#due-date", {
+    formatter: function (input, date, instance) {
+        var value = date.toLocaleDateString();
+        input.value = value;
+    }
+});
+var ToDoItem = (function () {
+    function ToDoItem(desiredTitle, dueDate, isComplete) {
+        this.title = desiredTitle;
+        this.dueDate = dueDate;
+        this.isComplete = isComplete;
+    }
+    return ToDoItem;
+}());
+var allToDoItemList = [];
+var completeItemList = [];
+var incompleteItemList = [];
+var completeLegendCount = 0;
+var incompleteLegendCount = 0;
+window.onload = function () {
+    var addBtn = getByID("addButton");
+    var updateBtn = getByID("updateButton");
+    var clearBtn = getByID("clearButton");
+    addBtn.addEventListener("click", clearErrMsg);
+    addBtn.addEventListener("click", main);
+    updateBtn.addEventListener("click", itemToggle);
+    clearBtn.addEventListener("click", clearLists);
+    specialKeyEventListener("title");
+    specialKeyEventListener("due-date");
+};
+function main() {
+    addToDoItem();
+    displayToDoItems();
+}
+function setCookies(cTitle, cDueDate, cIsComplete) {
+}
+function clearLists() {
+    allToDoItemList = [];
+    displayToDoItems();
+    clearErrMsg();
+    getByID("todoForm").reset();
+}
+function itemToggle() {
+    var itemDiv = this;
+    var index = itemDiv.getAttribute("data-index");
+    allToDoItemList[index].isComplete = !allToDoItemList[index].isComplete;
+    displayToDoItems();
+}
+function displayToDoItems() {
+    getByID("display-div").innerHTML = "";
+    allToDoItemList.sort(function (a, b) { return (a.dueDate >= b.dueDate) ? 1 : -1; });
+    var s;
+    for (var index in allToDoItemList) {
+        if (!allToDoItemList[index].isComplete) {
+            s = "incomplete";
+        }
+        else if (allToDoItemList[index].isComplete) {
+            s = "complete";
+        }
+        displayItem(s, allToDoItemList, index);
+    }
+}
+function separateItems(list) {
+    list.sort(function (a, b) { return (a.dueDate >= b.dueDate) ? 1 : -1; });
+    completeItemList = [];
+    incompleteItemList = [];
+    for (var i = 0; i < list.length; i++) {
+        if (list[i].isComplete) {
+            completeItemList.push(list[i]);
+        }
+        else {
+            incompleteItemList.push(list[i]);
+        }
+    }
+}
+function displayItem(s, list, index) {
+    var displayDiv = getByID("display-div");
+    var itemDiv = document.createElement("DIV");
+    itemDiv.ondblclick = itemToggle;
+    itemDiv.setAttribute("id", "todo-" + s + "-" + index);
+    itemDiv.setAttribute("data-index", index);
+    itemDiv.setAttribute("data-status", s);
+    itemDiv.classList.add("todo-" + s + "-" + index);
+    var itemTitle = document.createElement("h3");
+    if (list[index].isComplete) {
+        itemTitle.setAttribute("style", "text-decoration: line-through;");
+    }
+    itemTitle.innerText = list[index].title;
+    var itemDueDate = document.createElement("p");
+    itemDueDate.innerText = list[index].dueDate.toDateString();
+    displayDiv.appendChild(itemDiv);
+    itemDiv.appendChild(itemTitle);
+    itemDiv.appendChild(itemDueDate);
+}
+function getToDoItem() {
+    var title = getInputValueByID("title").trim();
+    var dueDate = getInputValueByID("due-date");
+    var isComplete = getInputByID("is-complete").checked;
+    var month = parseInt(dueDate.substring(0, dueDate.indexOf("/")));
+    var day = parseInt(dueDate.substring(dueDate.indexOf("/") + 1, dueDate.lastIndexOf("/")));
+    var year = parseInt(dueDate.substring(dueDate.lastIndexOf("/") + 1));
+    var item = new ToDoItem(title, new Date(year, month - 1, day), isComplete);
+    return item;
+}
+function addToDoItem() {
+    addInputEventToClearErrMsg();
+    if (isValid()) {
+        var item = getToDoItem();
+        allToDoItemList.push(item);
+        getByID("todoForm").reset();
+    }
+}
+function isValid() {
+    addInputEventToClearErrMsg();
+    var title = getInputValueByID("title").trim();
+    var dueDate = getInputValueByID("due-date").trim();
+    var month = parseInt(dueDate.substring(0, dueDate.indexOf("/")));
+    var day = parseInt(dueDate.substring(dueDate.indexOf("/") + 1, dueDate.lastIndexOf("/")));
+    var year = parseInt(dueDate.substring(dueDate.lastIndexOf("/") + 1));
+    var date = new Date(year, month - 1, day + 1);
+    var today = new Date();
+    if (title !== "" &&
+        dueDate !== "" &&
+        isValidDate(dueDate) &&
+        today < date) {
+        return true;
+    }
+    else {
+        if (title == "") {
+            castByID("title-err-msg").innerText = "Title can't be empty!";
+        }
+        if (dueDate == "") {
+            castByID("due-date-err-msg").innerText = "Due date can't be empty!";
+        }
+        if (dueDate !== "" && !isValidDate(dueDate)) {
+            castByID("due-date-err-msg").innerText = "Due date is not valid!";
+        }
+        if (today > date) {
+            castByID("due-date-err-msg").innerText = "The due date has been passed!";
+        }
+        return false;
+    }
+}
+function isValidDate(input) {
+    var pattern = /^\d{1,2}\/\d{1,2}\/\d{4}$/g;
+    var isCorrectFormat = pattern.test(input);
+    return isCorrectFormat;
+}
+function clearErrMsg() {
+    castByID("title-err-msg").innerText = "";
+    castByID("due-date-err-msg").innerText = "";
+}
+function addInputEventToClearErrMsg() {
+    getByID("title").addEventListener("input", clearErrMsg);
+    getByID("due-date").addEventListener("input", clearErrMsg);
+    getByID("is-complete").addEventListener("input", clearErrMsg);
+}
+function specialKeyEventListener(id) {
+    var input = getInputByID(id);
+    var addBtn = getByID("addButton");
+    input.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addBtn.click();
+        }
+        if (event.key === "Escape") {
+            event.preventDefault();
+            getByID("todoForm").reset();
+            clearErrMsg();
+        }
+    });
+}
+function getByID(id) {
+    return document.getElementById(id);
+}
+function getInputByID(id) {
+    return getByID(id);
+}
+function getInputValueByID(id) {
+    return getByID(id).value;
+}
+function castByID(id) {
+    return getByID(id);
+}
---
+++ ts/main.ts
@@ -0,0 +1,337 @@
+// @ts-ignore: ignoring issue with js-datepicker lack of intellisense
+
+// call datepicker from js library with format m/d/yyyy
+const picker = datepicker("#due-date", {
+    formatter: (input, date, instance) => {
+        const value = date.toLocaleDateString()
+        input.value = value // => '1/1/2099'
+    }
+});
+// set minimum past date
+// picker.setMin(new Date());
+
+class ToDoItem {
+    title: string;
+    dueDate: Date;
+    isComplete: boolean;
+
+    constructor(desiredTitle: string, dueDate: Date, isComplete: boolean) {
+        this.title = desiredTitle;
+        this.dueDate = dueDate;
+        this.isComplete = isComplete;
+    }
+}
+
+var allToDoItemList: ToDoItem[] = [];
+var completeItemList: ToDoItem[] = [];
+var incompleteItemList: ToDoItem[] = [];
+var completeLegendCount = 0;
+var incompleteLegendCount = 0;
+
+window.onload = function () {
+    let addBtn = <HTMLElement>getByID("addButton");
+    let updateBtn = <HTMLElement>getByID("updateButton");
+    let clearBtn = <HTMLElement>getByID("clearButton");
+    //addBtn.onclick = addToDoItem;
+
+    addBtn.addEventListener("click", clearErrMsg);
+    addBtn.addEventListener("click", main);
+    updateBtn.addEventListener("click", itemToggle);
+    clearBtn.addEventListener("click", clearLists);
+
+    //let grabChkBoxes = document.querySelectorAll("input[name=checkbox]");
+    //grabChkBoxes.addEventListener("click", main);
+
+    // button clicked when 'Enter' key pressed
+    // form reset and err msg cleared when 'ESC' key pressed
+    specialKeyEventListener("title");
+    specialKeyEventListener("due-date");
+
+    /* 
+        addBtn.onclick = () => {
+            clearErrMsg();
+            addToDoItem();
+        }
+    */
+}
+
+function main(): void {
+    addToDoItem();
+    displayToDoItems();
+}
+
+// Stores ToDoItems in cookies or web storage
+function setCookies(cTitle, cDueDate, cIsComplete): void {
+
+}
+
+function clearLists(): void {
+    allToDoItemList = [];
+    displayToDoItems();
+    clearErrMsg();
+    (<HTMLFormElement>getByID("todoForm")).reset();
+}
+
+// Allows user to mark a ToDoItem as complete or incomplete
+// moves complete and non-complete item between two lists
+function itemToggle(): void {
+
+    //mixedItemList = [];
+    var itemDiv = <HTMLElement>this;
+    let index = itemDiv.getAttribute("data-index");
+    allToDoItemList[index].isComplete = !allToDoItemList[index].isComplete;
+
+    displayToDoItems();
+}
+
+// display list of added ToDoItem
+function displayToDoItems(): void {
+    getByID("display-div").innerHTML = "";
+
+    // sort ToDoItems list by due date, most recent due date on top
+    allToDoItemList.sort((a, b) => (a.dueDate >= b.dueDate) ? 1 : -1);
+    //completeItemList.sort((a,b) => (a.dueDate > b.dueDate) ? 1 : ((b.dueDate > a.dueDate) ? -1 : 0));
+
+    //separateItems(allToDoItemList);
+    let s:string;
+
+    for (let index in allToDoItemList) {
+        if (!allToDoItemList[index].isComplete) {
+            s = "incomplete";
+        }
+        else if (allToDoItemList[index].isComplete) {
+            s = "complete";
+        }
+        displayItem(s, allToDoItemList, index);
+    }
+
+    /*
+    for (let index in incompleteItemList) {
+        displayItem("incomplete", incompleteItemList, index);
+    }
+
+    for (let index in completeItemList) {
+        displayItem("complete", completeItemList, index);
+    }
+    */
+}
+
+function separateItems(list: ToDoItem[]): void {
+    // sort ToDoItems list by due date, most recent due date on top
+    list.sort((a, b) => (a.dueDate >= b.dueDate) ? 1 : -1);
+    //completeItemList.sort((a,b) => (a.dueDate > b.dueDate) ? 1 : ((b.dueDate > a.dueDate) ? -1 : 0));
+
+    completeItemList = [];
+    incompleteItemList = [];
+
+    // separate complete and incomplete items to two lists
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].isComplete) {
+            completeItemList.push(list[i]);
+        }
+        else {
+            incompleteItemList.push(list[i]);
+        }
+    }
+}
+
+/**
+ * 
+ * @param s "complete" or "incomplete"
+ * @param list completeItemList or incompleteItemList
+ * @param index 
+ */
+function displayItem(s: string, list: ToDoItem[], index: string) {
+    let displayDiv = getByID("display-div");
+
+    let itemDiv = document.createElement("DIV");
+    itemDiv.ondblclick = itemToggle;
+    itemDiv.setAttribute("id", "todo-" + s + "-" + index);
+    itemDiv.setAttribute("data-index", index);
+    itemDiv.setAttribute("data-status", s);
+    itemDiv.classList.add("todo-" + s + "-" + index);
+
+    let itemTitle = document.createElement("h3");
+    if (list[index].isComplete) {
+        itemTitle.setAttribute("style", "text-decoration: line-through;");
+    }
+    itemTitle.innerText = list[index].title;
+
+    let itemDueDate = document.createElement("p");
+    itemDueDate.innerText = list[index].dueDate.toDateString();
+
+    displayDiv.appendChild(itemDiv);
+    itemDiv.appendChild(itemTitle);
+    itemDiv.appendChild(itemDueDate);
+
+}
+
+/**
+ * Get all input from the form and assign to a ToDoItem object
+ */
+function getToDoItem(): ToDoItem {
+    // get data from the form
+    let title = getInputValueByID("title").trim();
+    let dueDate = getInputValueByID("due-date");
+    let isComplete = getInputByID("is-complete").checked;
+
+    // get year, month, day from input to generate a date for object ToDoItem
+    let month = parseInt(dueDate.substring(0, dueDate.indexOf("/")));
+    let day = parseInt(dueDate.substring(dueDate.indexOf("/") + 1, dueDate.lastIndexOf("/")));
+    let year = parseInt(dueDate.substring(dueDate.lastIndexOf("/") + 1));
+
+    // Create item from input
+    let item = new ToDoItem(title, new Date(year, month - 1, day), isComplete);
+
+    return item;
+}
+
+/**
+ * add ToDoItem to database when all conditions are met
+ */
+function addToDoItem(): void {
+    addInputEventToClearErrMsg();
+    if (isValid()) {
+        let item = getToDoItem();
+        allToDoItemList.push(item);
+        (<HTMLFormElement>getByID("todoForm")).reset();
+    }
+}
+
+/**
+ * Checks form data is valid
+ */
+function isValid(): boolean {
+
+    addInputEventToClearErrMsg();
+    let title = getInputValueByID("title").trim();
+    let dueDate = getInputValueByID("due-date").trim();
+
+    let month = parseInt(dueDate.substring(0, dueDate.indexOf("/")));
+    let day = parseInt(dueDate.substring(dueDate.indexOf("/") + 1, dueDate.lastIndexOf("/")));
+    let year = parseInt(dueDate.substring(dueDate.lastIndexOf("/") + 1));
+
+    // JS counts month from 0, need to subtract 1 from month
+    // day counts at 00:00:00, add 1 to day to count the whole day until mid night
+    let date = new Date(year, month - 1, day + 1);
+    let today = new Date();
+
+    if (title !== "" &&
+        dueDate !== "" &&
+        isValidDate(dueDate) &&
+        today < date) {
+        return true;
+    }
+    else {
+        if (title == "") {
+            castByID("title-err-msg").innerText = "Title can't be empty!";
+        }
+        if (dueDate == "") {
+            castByID("due-date-err-msg").innerText = "Due date can't be empty!";
+        }
+        if (dueDate !== "" && !isValidDate(dueDate)) {
+            castByID("due-date-err-msg").innerText = "Due date is not valid!";
+        }
+        if (today > date) {
+            castByID("due-date-err-msg").innerText = "The due date has been passed!";
+        }
+        return false;
+    }
+}
+
+/**
+ * checks valid date input
+ * @param input date
+ * @returns true if input date is valid
+ */
+function isValidDate(input: string): boolean {
+    // Validating mm/dd/yyyy or m/d/yyyy
+    // \d{1,2}\/d{1,2}\/d{4}
+    let pattern = /^\d{1,2}\/\d{1,2}\/\d{4}$/g;
+    let isCorrectFormat = pattern.test(input);
+
+    return isCorrectFormat;
+}
+
+/**
+ * clear validation ul
+ */
+function clearErrMsg(): void {
+    castByID("title-err-msg").innerText = "";
+    castByID("due-date-err-msg").innerText = "";
+}
+
+/**
+ * function that clears error messages user starts typing 
+ */
+function addInputEventToClearErrMsg() {
+    getByID("title").addEventListener("input", clearErrMsg);
+    getByID("due-date").addEventListener("input", clearErrMsg);
+    getByID("is-complete").addEventListener("input", clearErrMsg);
+}
+
+/**
+ * execute functions when "Enter" or "ESC" key entered
+ * @param event of key pressed
+ */
+function specialKeyEventListener(id: string): void {
+    let input = getInputByID(id);
+    let addBtn = <HTMLElement>getByID("addButton");
+    // Execute a function when the user presses a key on the keyboard
+    input.addEventListener("keyup", function (event) {
+        // If the user presses the "Enter" key on the keyboard
+        if (event.key === "Enter") {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Trigger the button element with a click
+            addBtn.click();
+        }
+
+        if (event.key === "Escape") {
+            // Cancel the default action, if needed
+            event.preventDefault();
+            // Reset the form
+            (<HTMLFormElement>getByID("todoForm")).reset();
+            clearErrMsg();
+        }
+    });
+}
+
+/**
+ * short version of document.getElementById()
+ * @param id of HTML element
+ * @returns document.getElementById(id); 
+ */
+function getByID(id: string) {
+    return document.getElementById(id);
+}
+
+/**
+ * short version of (<HTMLInputElement>document.getElementById()).value
+ * @param id of input textbox
+ * @returns value of input textbox
+ */
+function getInputByID(id: string) {
+    return <HTMLInputElement>getByID(id);
+}
+
+/**
+ * short version of (<HTMLInputElement>document.getElementById()).value
+ * @param id of input textbox
+ * @returns value of input textbox
+ */
+function getInputValueByID(id: string) {
+    return (<HTMLInputElement>getByID(id)).value;
+}
+
+/**
+ * short version of (<HTMLElement>document.getElementById())
+ * @param id of HTML element
+ * @returns <HTMLElement>document.getElementById()
+ */
+function castByID(id: string) {
+    return (<HTMLElement>getByID(id));
+}
+
+
+
